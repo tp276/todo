7f8eb80e9ccb9341285f8da8cb38984a8aba4d90
sameer <200737tp276@gmail.com>
2021-03-29 01:01:59 +0530

    adding files

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -13,8 +13,7 @@ class Todo extends Component {
       date:'',
       taskDateOver:[],
       srcs:[],
-      flag:0,
-      data: []
+      flag:0
     }
   }
   
@@ -58,30 +57,11 @@ class Todo extends Component {
    if(this.state.todo !== '' && this.state.date !== '')
       this.checkTaskDate();
 
-
-    let temp_array = [];
-    let data_array=[];
-    let i,j;
-    for(i=0; i< this.state.todos.length; i++){
-      
-        temp_array.push(this.state.todos[i]);
-        temp_array.push(this.state.dates[i]);
-        temp_array.push(this.state.taskDateOver[i]);
-        temp_array.push(this.state.srcs[i]);
-      
-        data_array.push(temp_array);
-        temp_array=[];
-    }
-    const final = this.state.data;
-    final.push(data_array);
-    this.setState({
-      data:final
-    })
     }
 
   checkTaskDate = ()=>{
 
-      console.log('printing the dates array',this.state.dates);
+      //console.log('printing the dates array',this.state.dates);
       let today = new Date();
       let curr_date = today.getDate();
       let curr_month = today.getMonth() + 1;
@@ -90,7 +70,7 @@ class Todo extends Component {
       let p = curr_full_date.split('-');
       let md = new Date(p[0], p[1] - 1, p[2]);
       let todayDate = md.toDateString();
-      console.log('current date',todayDate);
+      //console.log('current date',todayDate);
         
         let taskDateCheck= [];
         let length = this.state.dates.length;
@@ -100,16 +80,16 @@ class Todo extends Component {
         let parts = date.split('-');
         let mydate = new Date(parts[0], parts[1] -1, parts[2]);
         let taskDate = mydate.toDateString();
-        console.log('date fetched from array',taskDate);
+        //console.log('date fetched from array',taskDate);
          
 
         taskDateCheck = this.state.taskDateOver;
         
 
         var g1 = new Date(todayDate);
-        console.log('g1 = ',g1);
+        //console.log('g1 = ',g1);
         var g2 = new Date(taskDate);
-        console.log('g2=', g2);
+        //console.log('g2=', g2);
 
         if (g1.getTime() < g2.getTime())
           taskDateCheck.push(false);
@@ -117,12 +97,12 @@ class Todo extends Component {
           taskDateCheck.push(false);  
         if (g1.getTime() > g2.getTime())
           taskDateCheck.push(true);  
-      this.setState({
-        taskDateOver: taskDateCheck
-      })
-      console.log('printing the length of the taskDateOver array',this.state.taskDateOver.length);
+        this.setState({
+          taskDateOver: taskDateCheck
+        })
+      //console.log('printing the length of the taskDateOver array',this.state.taskDateOver.length);
       this.state.taskDateOver.map( res =>{
-      console.log("task date over array each element: ",res);
+      //console.log("task date over array each element: ",res);
     });
   }
 
@@ -196,24 +176,36 @@ class Todo extends Component {
         <p className="lead text-center">This is a simple to app to keep your life easy...</p>      
         </Jumbotron>
         
+        <Container>
         
         <form className="input" onSubmit={this.handleSubmit} className="text-center">
+        <Row>
+          <Col sm= "auto" >
           <input type="text" onChange={(event) =>{ this.setState({ todo: event.target.value})}}/>&nbsp;&nbsp;
+          </Col>
+          <Col sm= "auto" >
           <label>Finish date</label>&nbsp;
           <input type = "date"
             onChange={(event)=>{this.setState({date: event.target.value})}}/>&nbsp;&nbsp;
+          </Col>
+          <Col sm= "auto" >
           <input type="file" 
-          className="btn btn-lg btn-primary" 
+          className="btn btn-sm btn-primary" 
           id="file-selector"
           accept=".jpg, .jpeg, .png"
           onChange={(e) => this.getImage(e)}/> &nbsp;&nbsp;
-          <input type="submit" value="Add" className="btn btn-lg btn-primary"/>
-
+          </Col>
+          <Col sm= "auto">
+          <input type="submit" value="Add" className="btn btn-sm btn-primary"/>
+          </Col>
           <ToastContainer
           position="bottom-right" 
           autoClose={2000}/>
+           </Row>
         </form>
-      
+       
+        </Container>
+
         <br/>
         <br/>
         <div className="todoList">
@@ -232,22 +224,22 @@ class Todo extends Component {
                   
                   
                   <Row>
-                  <Col style={{fontWeight:'bold'}}>
+                  <Col style={{fontWeight:'bold'}} lg={3}>
                   
                   {todo}
                   </Col>
-                  <Col>
+                  <Col lg={3}>
                     finish by: &nbsp; 
                   {this.state.dates[index]}&nbsp;</Col>
                   
-                  <Col>
+                  <Col lg={3}>
                     {
                     (this.state.taskDateOver[index] == true) ? 
                     <p style={{color:'red'}}>Task date over</p>: <p>Task pending</p>
                     }
                   </Col>
                   
-                  <Col>
+                  <Col lg={3}>
                       <img src = {this.state.srcs[index]} height= "200"/>
                   </Col>
 
